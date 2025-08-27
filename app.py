5e86cd3cdecac2cce32b5b1a0373035fa825b196
valdomitchell-web <valdomitchell@gmail.com>
2025-08-26 20:53:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -111,8 +111,8 @@ NOTIFY_URL = os.getenv("NOTIFY_URL")  # e.g. https://hooks.example/abc123
 # Binance client
 # =========================
 def get_binance_client() -> Client:
-    c = make_client()
-try:
+        c = make_client()
+    try:
         server_time = client.get_server_time()["serverTime"]  # ms
         local_ms = int(time.time() * 1000)
         client.TIME_OFFSET = server_time - local_ms
